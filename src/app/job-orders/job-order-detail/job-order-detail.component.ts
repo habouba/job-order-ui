59c4509f62f7ad6dba193935e994ebaab286a940
Habib Ben Kram <h_habouba@hotmail.com>
2019-09-22 19:34:17 -0400

    display error messages

--- src/app/job-orders/job-order-detail/job-order-detail.component.ts
+++ src/app/job-orders/job-order-detail/job-order-detail.component.ts
@@ -7,10 +7,12 @@ import {
   OnChanges,
   Output,
   SimpleChanges,
-  ViewChild
+  ViewChild,
+  OnInit
 } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { JobOrder } from "../../core";
+import { GenericValidator } from "../../shared/generic-validator";
 
 @Component({
   selector: "app-job-order-detail",
@@ -18,7 +20,7 @@ import { JobOrder } from "../../core";
   styleUrls: ["./job-order-detail.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class JobOrderDetailComponent implements OnChanges {
+export class JobOrderDetailComponent implements OnInit, OnChanges {
   @Input() jobOrder: JobOrder;
   @Output() unselect = new EventEmitter<string>();
   @Output() add = new EventEmitter<JobOrder>();
@@ -27,14 +29,54 @@ export class JobOrderDetailComponent implements OnChanges {
   @ViewChild("name", { static: true }) nameElement: ElementRef;
 
   addMode = false;
+  form: FormGroup;
 
-  form = this.fb.group({
-    id: [],
-    name: ["", Validators.required],
-    saying: [""]
-  });
+  // Use with the generic validation message class
+  displayMessage: { [key: string]: string } = {};
+  private validationMessages: { [key: string]: { [key: string]: string } };
+  private genericValidator: GenericValidator;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id: [],
+      name: [
+        "",
+        [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      ],
+      saying: ["", Validators.required]
+    });
+
+    // Defines all of the validation messages for the form.
+    // These could instead be retrieved from a file or database.
+    this.validationMessages = {
+      name: {
+        required: "Name is required.",
+        minlength: "Name must be at least three characters.",
+        maxlength: "Name cannot exceed 50 characters."
+      },
+      saying: {
+        required: "saying is required."
+      }
+    };
+
+    // Define an instance of the validator for use with this form,
+    // passing in this form's set of validation messages.
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+
+  ngOnInit(): void {
+    // Watch for value changes
+    this.form.valueChanges.subscribe(
+      value =>
+        (this.displayMessage = this.genericValidator.processMessages(this.form))
+    );
+  }
+
+  // Also validate on blur
+  // Helpful if the user tabs through required fields
+  blur(): void {
+    this.displayMessage = this.genericValidator.processMessages(this.form);
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     this.setFocus();
@@ -45,6 +87,12 @@ export class JobOrderDetailComponent implements OnChanges {
       this.form.reset();
       this.addMode = true;
     }
+  }
+
+  getErrorMessage() {
+    return this.form.get("saying").hasError("required")
+      ? "You must enter a value"
+      : "";
   }
 
   addJobOrder(form: FormGroup) {
