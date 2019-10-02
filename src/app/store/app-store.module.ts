9ade0853635633f5135f77d4ed8aefc16131918e
Habib Ben Kram <h_habouba@hotmail.com>
2019-10-01 20:39:47 -0400

    custom entities data with custom url from server

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { NgrxDataModule, EntityDataService } from "ngrx-data";
+import {
+  NgrxDataModule,
+  EntityDataService,
+  DefaultDataServiceConfig,
+  EntityCollectionDataService,
+  HttpUrlGenerator,
+  QueryParams,
+  HttpMethods,
+  Update,
+  EntityHttpResourceUrls
+} from "ngrx-data";
 import { environment } from "../../environments/environment";
 import { entityConfig } from "./entity-metadata";
 import { NgrxDataToastService } from "./ngrx-data-toast.service";
 import { JobOrderDataService } from "./entity/job-order-data-service";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @NgModule({
   imports: [
@@ -22,10 +35,40 @@ import { JobOrderDataService } from "./entity/job-order-data-service";
 export class AppStoreModule {
   constructor(
     entityDataService: EntityDataService,
-    jobOrderDataService: JobOrderDataService,
-    toastService: NgrxDataToastService
+    http: HttpClient,
+    httpUrlGenerator: HttpUrlGenerator
   ) {
-    // Register custom EntityDataServices
-    entityDataService.registerService("Joborder", jobOrderDataService); // <-- register it
+    http
+      .get("https://api.github.com/users/habouba/repos")
+      .pipe(
+        tap(val => {
+          /*testServ.setConf(
+            {
+              root: val[0].owner.url,
+              entityHttpResourceUrls: {
+                Joborder: {
+                  // You must specify the root as part of the resource URL.
+                  entityResourceUrl: val[0].owner.url,
+                  collectionResourceUrl: val[0].owner.url
+                }
+              }
+            });*/
+
+          //httpUrlGenerator.collectionResource('Joborder','api');
+          //httpUrlGenerator.entityResource('Joborder','kakak/ttt');
+          var g = {
+            Joborder: {
+              // You must specify the root as part of the resource URL.
+              entityResourceUrl: "api/joborder/",
+              collectionResourceUrl: "api/joborders"
+            }
+          } as EntityHttpResourceUrls;
+          httpUrlGenerator.registerHttpResourceUrls(g);
+          var t = new JobOrderDataService(http, httpUrlGenerator);
+          // Register custom EntityDataServices
+          entityDataService.registerService("Joborder", t); // <-- register it
+        })
+      )
+      .subscribe();
   }
 }
--- src/app/store/entity/job-order-data-service.ts
+++ src/app/store/entity/job-order-data-service.ts
@@ -5,45 +5,15 @@ import {
   DefaultDataService,
   HttpUrlGenerator,
   Logger,
-  QueryParams
+  QueryParams,
+  DefaultDataServiceConfig
 } from "ngrx-data";
 
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 import { JobOrder } from "../../core";
 
 @Injectable()
 export class JobOrderDataService extends DefaultDataService<JobOrder> {
-  constructor(
-    http: HttpClient,
-    httpUrlGenerator: HttpUrlGenerator,
-    logger: Logger
-  ) {
+  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
     super("Joborder", http, httpUrlGenerator);
-    logger.log("Created custom Job Order EntityDataService");
-  }
-
-  getAll(): Observable<JobOrder[]> {
-    return super
-      .getAll()
-      .pipe(
-        map(Joborders => Joborders.map(joborder => this.mapJoborder(joborder)))
-      );
-  }
-
-  getById(id: string | number): Observable<JobOrder> {
-    return super.getById(id).pipe(map(joborder => this.mapJoborder(joborder)));
-  }
-
-  getWithQuery(params: string | QueryParams): Observable<JobOrder[]> {
-    return super
-      .getWithQuery(params)
-      .pipe(
-        map(jobOrders => jobOrders.map(jobOrder => this.mapJoborder(jobOrder)))
-      );
-  }
-
-  private mapJoborder(jobOrder: JobOrder): JobOrder {
-    return { ...jobOrder, dateLoaded: new Date() };
   }
 }
